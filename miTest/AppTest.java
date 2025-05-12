a271bdec6241aea07f1823b0c717801c2457744a
kevinjassiel5f2 <l24170733@culiacan.tecnm.mx>
2025-05-12 16:53:52 +0000

    fwfw

--- miTest/AppTest.java
+++ miTest/AppTest.java
@@ -7,9 +7,5 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class AppTest {
 
-    @Test 
-    void appHasAGreeting() {
-        Principal classUnderTest = new Principal();
-        assertNotNull(classUnderTest.getGreeting(), "app should have a greeting");
-    }
+    
 }
